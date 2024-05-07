517dc389fe11c206597dd388d6235676894d8972
Vipul-28 <vipuldongre348@gmail.com>
2024-05-07 23:05:22 +0530

    7th of may

--- doubleIt.cpp
+++ doubleIt.cpp
@@ -8,7 +8,6 @@
 
 // Example 1:
 
-
 // Input: head = [1,8,9]
 // Output: [3,7,8]
 // Explanation: The figure above corresponds to the given linked list which represents the number 189. Hence, the returned linked list represents the number 189 * 2 = 378.
